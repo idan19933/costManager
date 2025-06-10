4dcba8706eb8ec14bc0910d40694800af30ae9f6
hshan <hshany@gmail.com>
2025-06-10 20:33:45 +0300

    fixed project

--- models/user.js
+++ models/user.js
@@ -2,16 +2,27 @@
 
 const mongoose = require('mongoose');
 
-
+/**
+ * User schema definition for MongoDB collection
+ * @typedef {Object} User
+ * @property {string} id - Unique user identifier
+ * @property {string} first_name - User's first name
+ * @property {string} last_name - User's last name
+ * @property {Date} birthday - User's birth date
+ * @property {string} marital_status - User's marital status
+ */
 const userSchema = new mongoose.Schema(
     {
-        id: { type: String, required: true, index: true, unique: true },
-        first_name: { type: String, required: true },
-        last_name: { type: String, required: true },
-        birdName: { type: Date, required: true },
-        marital_status: { type: String, required: true },
+            id: { type: String, required: true, index: true, unique: true },
+            first_name: { type: String, required: true }, // user's first name
+            last_name: { type: String, required: true }, // user's last name
+            birthday: { type: Date, required: true }, // user's birthday
+            marital_status: { type: String, required: true }, // user's marital status
     }
 );
 
-
-module.exports = mongoose.model('User', userSchema);
+/**
+ * Exports the User model for use in the application
+ * @module User
+ */
+module.exports = mongoose.model('User', userSchema); // export the User model for use in the application
--- test/api.test.js
+++ test/api.test.js
@@ -1,39 +1,59 @@
+
+
 const request = require('supertest');
 const chai = require('chai');
-const app = require('../app'); // your Express app
+const app = require('../app');
 
 const expect = chai.expect;
 
+/**
+ * Test suite for Cost Manager API endpoints.
+ */
 describe('Cost Manager API Tests', () => {
 
+    /**
+     * Test GET /api/about endpoint
+     * Expects status 200 and an array of team info objects
+     */
     it('GET /api/about - should return team info', async () => {
-        const res = await request(app).get('/api/about');
-        expect(res.status).to.equal(200);
-        expect(res.body).to.be.an('array');
+        const res = await request(app).get('/api/about'); // Send a GET request to the /api/about endpoint
+        expect(res.status).to.equal(200); // Assert that the response status is 200 (OK)
+        expect(res.body).to.be.an('array'); // Assert that the response body is an array
     });
 
+    /**
+     * Test POST /api/add endpoint
+     * Sends a new cost item and expects status 200 and response to contain 'description' property
+     */
     it('POST /api/add - should add a cost', async () => {
-        const res = await request(app).post('/api/add').send({
+        const res = await request(app).post('/api/add').send({ // Send a POST request with data in the request body
             userid: 123123,
             description: 'milk test',
             category: 'food',
             sum: 10
         });
 
-        expect(res.status).to.equal(200);
-        expect(res.body).to.have.property('description');
+        expect(res.status).to.equal(200); // Assert that the response status is 200 (OK)
+        expect(res.body).to.have.property('description'); // Assert that the response body has a property named 'description'
     });
 
+    /**
+     * Test GET /api/report endpoint
+     * Requests a report for given user/year/month and expects status 200 and 'costs' property in response
+     */
     it('GET /api/report - should return report', async () => {
-        const res = await request(app).get('/api/report?id=123123&year=2025&month=5');
-        expect(res.status).to.equal(200);
-        expect(res.body).to.have.property('costs');
+        const res = await request(app).get('/api/report?id=123123&year=2025&month=5'); // Send a GET request with parameters in the query string
+        expect(res.status).to.equal(200); // Assert that the response status is 200 (OK)
+        expect(res.body).to.have.property('costs'); // Assert that the response body has a property named 'costs'
     });
+});
 
-    it('GET /api/users/:id - should return user data', async () => {
-        const res = await request(app).get('/api/users/123123');
-        expect(res.status).to.equal(200);
-        expect(res.body).to.have.property('first_name');
-    });
-
+/**
+ * Test GET /api/users/:id endpoint
+ * Requests user info and expects status 200 and response to have 'first_name' property
+ */
+it('GET /api/users/:id - should return user data', async () => {
+    const res = await request(app).get('/api/users/123123'); // Send a GET request with the ID as part of the path
+    expect(res.status).to.equal(200);// Assert that the response status is 200 (OK)
+    expect(res.body).to.have.property('first_name'); // Assert that the response body has a property named 'first_name'
 });
